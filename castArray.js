4483d2520e004c508ac98fa203f0e45ac2e6cd7c
imcuttle <moyuyc95@gmail.com>
2020-04-03 16:07:46 +0800

    fix: html-collection is not iterable in edge

--- castArray.js
+++ castArray.js
@@ -6,12 +6,14 @@
  */
 const isIterable = require('is-iterable')
 
+const collListTypes = [global.HTMLCollection, global.NodeList].filter(x => typeof x === 'function')
+
 module.exports = function(input) {
   if (input && typeof input.slice === 'function') {
     return input
   }
 
-  if (isIterable(input)) {
+  if (isIterable(input) || collListTypes.find(type => input instanceof type)) {
     return [].slice.apply(input)
   }
 
